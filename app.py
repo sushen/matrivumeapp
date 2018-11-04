cb19b52e3e9a11a6b83a6144ff624ff2e8ffc9c9
sushen <sushenbiswasaga@gmail.com>
2018-11-04 12:33:40 +0600

    Requirment.txt and Procfile is ready

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "This is my matrivume and I want smooth piplie"
+    return "This is my matrivume and I want smooth piplie angagement"
 
 
 if __name__ == "__main__":
